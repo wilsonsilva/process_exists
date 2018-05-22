7d1dbd8831ff2136efecd8ee60ae480d7e7b3149
Wilson Silva <wilson.dsigns@gmail.com>
2018-05-22 22:36:40 +0100

    Replace double quotes with single quotes

--- spec/process_exists_spec.rb
+++ spec/process_exists_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'process_exists'
 
 describe ProcessExists do
-  it "has a VERSION constant" do
+  it 'has a VERSION constant' do
     expect(subject.const_get('VERSION')).not_to be_empty
   end
 end
