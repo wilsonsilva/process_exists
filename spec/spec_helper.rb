13c91dc5cd0451cb4a76c614c6b0843e8c501f10
Wilson Silva <wilson.dsigns@gmail.com>
2018-05-22 21:59:27 +0100

    Cleanup the spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,7 @@ require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
 require 'rspec'
-require 'process_exists/version'
-
-include ProcessExists
+require 'process_exists'
 
 def running_specs_as_root?
   # The superuser normally has a UID of zero (0)
